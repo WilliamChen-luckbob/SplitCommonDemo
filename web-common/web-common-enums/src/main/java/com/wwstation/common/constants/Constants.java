352626503aa70d5300497502be6e685f4f3802d8
WilliamChen-luckbob <416934181@qq.com>
2022-01-28 14:02:54 +0800

    update

---
+++ web-common/web-common-enums/src/main/java/com/wwstation/common/constants/Constants.java
@@ -0,0 +1,18 @@
+package com.wwstation.common.constants;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 常量值
+ *
+ * @author william
+ * @description
+ * @Date: 2021-02-17 11:56
+ */
+@Getter
+@AllArgsConstructor
+public class Constants {
+    public static final String commonString="commonString";
+
+}
---
+++ web-common/web-common-model/ref-model/src/main/java/com/wwstation/common/TestClass.java
@@ -0,0 +1,14 @@
+package com.wwstation.common;
+
+import com.wwstation.common.constants.Constants;
+
+/**
+ * @author william
+ * @description
+ * @Date: 2022-01-28 12:04
+ */
+public class TestClass {
+    public void test() {
+        System.out.println(Constants.commonString);
+    }
+}
